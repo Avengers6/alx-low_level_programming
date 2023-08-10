46d6079b55c69e9533a76efe556767fe41855b9c
avengers6 <mikeykim28@gmail.com>
2023-08-10 12:03:29 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line too a standard error
- * return 1
+ * main - A c program that prints a line to the standard error
+ * return 1 (Success)
  */
-int min(void)
+int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
